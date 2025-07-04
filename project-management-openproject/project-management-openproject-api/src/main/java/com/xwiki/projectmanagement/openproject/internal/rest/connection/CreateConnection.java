cfab07bf07b360da764ed652812ab1fbdc8a21b7
Rares-Emil Bancescu <rares.bancescu@xwiki.com>
2025-07-04 17:10:44 +0300

    Implement connections design and change connection handling logic wip

---
+++ project-management-openproject/project-management-openproject-api/src/main/java/com/xwiki/projectmanagement/openproject/internal/rest/connection/CreateConnection.java
@@ -0,0 +1,72 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package com.xwiki.projectmanagement.openproject.internal.rest.connection;
+
+import java.util.Map;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.xwiki.component.annotation.Component;
+import org.xwiki.rest.XWikiResource;
+
+import com.xwiki.projectmanagement.openproject.internal.service.CreateConnectionService;
+
+/**
+ * Create connection REST resource.
+ *
+ * @version $Id$
+ */
+@Component
+@Named("com.xwiki.projectmanagement.openproject.internal.rest.connection.CreateConnection")
+@Path("openproject/connections")
+public class CreateConnection extends XWikiResource
+{
+    @Inject
+    private CreateConnectionService createConnectionService;
+    /**
+     * @param data dasdsads
+     * @return things.
+     */
+    @POST
+    @Path("/create")
+    @Consumes({ MediaType.APPLICATION_JSON })
+    @Produces({ MediaType.APPLICATION_JSON })
+    public Response createConnection(Map<String, Object> data)
+    {
+        String connectionName = (String) data.get("connectionName");
+        String serverURL = (String) data.get("serverURL");
+        String clientId = (String) data.get("clientId");
+        String clientSecret = (String) data.get("clientSecret");
+        try {
+            createConnectionService.createConnection(connectionName, serverURL, clientId, clientSecret);
+        } catch (Exception e) {
+            return Response.status(Response.Status.CONFLICT).entity(e.getMessage()).build();
+        }
+
+        return Response.ok().build();
+    }
+}
---
+++ project-management-openproject/project-management-openproject-api/src/main/java/com/xwiki/projectmanagement/openproject/internal/service/CreateConnectionService.java
@@ -0,0 +1,119 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package com.xwiki.projectmanagement.openproject.internal.service;
+
+import java.util.Arrays;
+
+import javax.inject.Inject;
+import javax.inject.Provider;
+import javax.inject.Singleton;
+
+import org.xwiki.bridge.DocumentAccessBridge;
+import org.xwiki.component.annotation.Component;
+import org.xwiki.model.reference.DocumentReference;
+
+import com.xpn.xwiki.XWikiContext;
+import com.xpn.xwiki.doc.XWikiDocument;
+import com.xpn.xwiki.objects.BaseObject;
+
+/**
+ * Class for creating the connection.
+ *
+ * @version $Id$
+ */
+@Component(roles = CreateConnectionService.class)
+@Singleton
+public class CreateConnectionService
+{
+    private static final String CONNECTION_NAME = "connectionName";
+
+    private static final String SERVER_URL = "serverURL";
+
+    private static final String CLIENT_ID = "clientId";
+
+    private static final String CLIENT_SECRET = "clientSecret";
+
+    private static final String OPEN_PROJECT_CONFIGURATIONS = "OpenProjectConfigurations";
+
+    private static final String PROJECT_MANAGEMENT = "ProjectManagement";
+
+    private static final String INSTANCE_CONFIGURATION = "InstanceConfiguration";
+
+    @Inject
+    private DocumentAccessBridge documentAccessBridge;
+
+    @Inject
+    private Provider<XWikiContext> xcontextProvider;
+
+    /**
+     * Creates a new OpenProject connection configuration document in XWiki.
+     * @param connectionName the unique name used to identify the connection. It will be used as part of the
+     *     document name.
+     * @param serverURL the base URL of the OpenProject server.
+     * @param clientId the OAuth2 client ID used for authentication with OpenProject.
+     * @param clientSecret the OAuth2 client secret used for authentication with OpenProject.
+     * @throws Exception if there is any issue while creating or saving the document, or accessing the context.
+     */
+    public void createConnection(String connectionName, String serverURL, String clientId,
+        String clientSecret) throws Exception
+    {
+        XWikiContext context = this.xcontextProvider.get();
+        String wikiName = context.getWikiId();
+
+        DocumentReference docRef = new DocumentReference(
+            wikiName,
+            Arrays.asList(PROJECT_MANAGEMENT, OPEN_PROJECT_CONFIGURATIONS),
+            connectionName + INSTANCE_CONFIGURATION
+        );
+
+        if (documentAccessBridge.exists(docRef)) {
+            throw new RuntimeException("Connection already exists with name: " + connectionName);
+        }
+
+        XWikiDocument doc = context.getWiki().getDocument(docRef, context);
+        doc.setTitle(connectionName + INSTANCE_CONFIGURATION);
+        doc.setHidden(true);
+
+        DocumentReference configClassRef =
+            new DocumentReference(wikiName, PROJECT_MANAGEMENT, "OpenProjectConnectionClass");
+        BaseObject configObj = doc.getXObject(configClassRef, true, context);
+        configObj.setStringValue(CONNECTION_NAME, connectionName);
+        configObj.setStringValue(SERVER_URL, serverURL);
+        configObj.setStringValue(CLIENT_ID, clientId);
+        configObj.setStringValue(CLIENT_SECRET, clientSecret);
+
+        DocumentReference oidcClassRef = new DocumentReference(wikiName, "XWiki", "OIDC.ClientConfigurationClass");
+
+        BaseObject oidcObj = doc.getXObject(oidcClassRef, true, context);
+        oidcObj.setStringValue("configurationName", connectionName);
+        oidcObj.setStringValue("authorizationEndpoint", serverURL + "/oauth/authorize");
+        oidcObj.setStringValue("tokenEndpoint", serverURL + "/oauth/token");
+        oidcObj.setStringValue(CLIENT_ID, clientId);
+        oidcObj.setStringValue(CLIENT_SECRET, clientSecret);
+        oidcObj.setStringValue("tokenEndpointMethod", "client_secret_basic");
+        oidcObj.setIntValue("skipped", 0);
+        oidcObj.setStringValue("scope", "api_v3");
+        oidcObj.setStringValue("responseType", "code");
+        oidcObj.setIntValue("enableUser", 1);
+        oidcObj.setStringValue("tokenStorageScope", "USER");
+
+        context.getWiki().saveDocument(doc, "Created OpenProject and OIDC config via REST", context);
+    }
+}
